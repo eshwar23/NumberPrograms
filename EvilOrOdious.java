ae08e3423eb6365287b1411f8664c615c52e3f8d
eshwar23 <enigma.bluewater@gmail.com>
2023-06-28 11:32:59 +0530

    Add files via upload

---
+++ EvilOrOdious.java
@@ -0,0 +1,60 @@
+import java.util.*;
+class EvilOrOdious
+{
+    int num;
+    int bin;
+    EvilOrOdious()
+    {
+        num=0;
+        bin=0;
+    }
+    void acceptNum()
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a positive number");
+        num=in.nextInt();
+        binary(num);
+    }
+    void binary(int x)
+    {
+        int d,p=0;
+        while(x>0)
+        {
+            d=x%2;
+            bin=bin+d*(int)Math.pow(10,p);
+            p=p+1;
+            x=x/2;
+        }
+        System.out.println("The binary value is  "+ bin);
+    }
+    void check()
+    {
+        int c=0,d;
+        while(bin>0)
+        {
+            d=bin%10;
+            if(d==1)
+            {
+                c=c+1;
+            }
+            bin=bin/10;
+        }
+        if(c%2==0)
+        {
+            System.out.println("Evil number ");
+        }
+        else
+        {
+            System.out.println("Odious Number");
+        }
+    }
+    public static void main()
+    {
+        EvilOrOdious obj=new EvilOrOdious();
+        obj.acceptNum();
+        obj.check();
+    }
+}
+    
+    
+    
